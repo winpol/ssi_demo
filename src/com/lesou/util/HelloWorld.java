00294fc54c42613e2426517d062bb8bc8cab5310
winpol <win_pol@163.com>
2016-03-23 18:12:57 +0800

    send

---
+++ src/com/lesou/util/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.lesou.util;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+		System.out.println("i am pol");
+	}
+
+}
