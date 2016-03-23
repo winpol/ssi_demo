b3aeb37e6e5f70ef8626ff6dd7a711616b4432d3
winpol <win_pol@163.com>
2016-03-23 18:16:19 +0800

    three commit

---
+++ src/com/lesou/dao/TestDao.java
@@ -0,0 +1,14 @@
+package com.lesou.dao;
+
+public class TestDao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome...");
+	}
+
+}
