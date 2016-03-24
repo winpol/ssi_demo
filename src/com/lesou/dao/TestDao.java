a9bc19078855d784438f1f4d6b0a395d8337b67c
winpol <win_pol@163.com>
2016-03-24 20:33:25 +0800

    4

--- src/com/lesou/dao/TestDao.java
+++ src/com/lesou/dao/TestDao.java
@@ -12,6 +12,7 @@ public class TestDao {
 		System.out.println("first update");
 		System.out.println("2 time update");
 		System.out.println("3 time abc");
+		System.out.println("4 time aaa");
 	}
 
 }
