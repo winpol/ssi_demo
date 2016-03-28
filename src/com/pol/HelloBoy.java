1ca0619eabb1a47416d3f31d951a83054496d480
winpol <win_pol@163.com>
2016-03-28 16:09:35 +0800

    add by wxb

--- src/com/pol/HelloBoy.java
+++ src/com/pol/HelloBoy.java
@@ -8,7 +8,7 @@ public class HelloBoy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Boy");
+		System.out.println("Hello Boy add by wxb");
 	}
 
 }
