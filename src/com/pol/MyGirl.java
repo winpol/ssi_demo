1dca8659230a7b8cb3181cb76c3a3a1fe264a9d9
winpol <win_pol@163.com>
2016-03-28 15:36:51 +0800

    test qq com

---
+++ src/com/pol/MyGirl.java
@@ -0,0 +1,14 @@
+package com.pol;
+
+public class MyGirl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my girl info");
+	}
+
+}
