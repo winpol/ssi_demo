9963e90ebfc649b1b3b4b2000ad9ec3d6b9c851c
winpol <win_pol@163.com>
2016-03-28 15:45:42 +0800

    hello boy

---
+++ src/com/pol/HelloBoy.java
@@ -0,0 +1,14 @@
+package com.pol;
+
+public class HelloBoy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Boy");
+	}
+
+}
